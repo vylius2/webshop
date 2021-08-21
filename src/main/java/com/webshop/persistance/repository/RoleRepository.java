eb7da55fa9c91a9827671acdb14da5d2cb034bd8
Vilius <polerisviliux@gmail.com>
2021-08-21 10:18:37 +0300

    Repository annotations added

--- src/main/java/com/webshop/persistance/repository/RoleRepository.java
+++ src/main/java/com/webshop/persistance/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package com.webshop.persistance.repository;
 
 import com.webshop.persistance.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
---
+++ src/main/java/com/webshop/service/CommentService.java
@@ -0,0 +1,6 @@
+package com.webshop.service;
+
+public class CommentService {
+
+
+}
