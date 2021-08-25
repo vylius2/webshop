56e12839f980c8a593bd2c0eb6dbb0bc6c3c6b5b
Vilius <polerisviliux@gmail.com>
2021-08-25 17:55:04 +0300

    api operations and api responses added for swagger documentation

--- src/main/java/com/webshop/api/controller/CommentController.java
+++ src/main/java/com/webshop/api/controller/CommentController.java
@@ -8,6 +8,9 @@ import com.webshop.exception.ProductNotFound;
 import com.webshop.persistance.entity.Comment;
 import com.webshop.service.CommentService;
 import com.webshop.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -27,21 +30,39 @@ public class CommentController {
         this.productService = productService;
     }
 
+    @ApiOperation(value = "Get all comments", tags = "getAll", httpMethod = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully get list of comments"),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public List<Comment> getAllComments(){
+    public List<Comment> getAll(){
         return commentService.getComments();
     }
 
+    @ApiOperation(value = "Get all comments", tags = "getAllCommentsByProduct", httpMethod = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully get list of comments"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Product does not exist")
+    })
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('USER')")
-    public List<Comment> getAllCommentByProduct(@PathVariable("id") Long id){
+    public List<Comment> getAllCommentsByProduct(@PathVariable("id") Long id){
         if (!productService.existsById(id)){
             throw new ProductNotFound(id);
         }
         return commentService.getAllCommentByProduct(id);
     }
 
+    @ApiOperation(value = "Create comment", tags = "create", httpMethod = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created comment record"),
+            @ApiResponse(code = 400, message = "Validation failed"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Product does not exist")
+    })
     @PostMapping("/product/{productId}")
     @PreAuthorize("hasRole('USER')")
     public CreateCommentResponse create(@PathVariable Long productId,
@@ -49,6 +70,12 @@ public class CommentController {
         return new CreateCommentResponse(commentService.save(createCommentRequest, productId));
     }
 
+    @ApiOperation(value = "Delete comment by id", tags = "delete", httpMethod = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Successfully delete comment by id"),
+            @ApiResponse(code = 404, message = "Comment not found error"),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
     @DeleteMapping("/{commentId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("hasRole('ADMIN')")
@@ -56,6 +83,12 @@ public class CommentController {
         commentService.delete(commentId);
     }
 
+    @ApiOperation(value = "Update comment", tags = "update", httpMethod = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated comment record"),
+            @ApiResponse(code = 400, message = "Validation failed"),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('USER')")
     public UpdateCommentResponse update(@PathVariable("id") Long id,
--- src/main/java/com/webshop/api/controller/ProductController.java
+++ src/main/java/com/webshop/api/controller/ProductController.java
@@ -6,6 +6,9 @@ import com.webshop.api.dto.response.CreateProductResponse;
 import com.webshop.api.dto.response.UpdateProductResponse;
 import com.webshop.persistance.entity.Product;
 import com.webshop.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -25,12 +28,23 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @ApiOperation(value = "Get all products", tags = "getAll", httpMethod = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully get list of products"),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
     @GetMapping
     @PreAuthorize("hasRole('USER')")
     public List<Product> getAll(){
         return productService.getAll();
     }
 
+    @ApiOperation(value = "Create product", tags = "create", httpMethod = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created product record"),
+            @ApiResponse(code = 400, message = "Validation failed"),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('ADMIN')")
@@ -38,6 +52,12 @@ public class ProductController {
         return new CreateProductResponse(productService.create(new Product(createProductRequest)));
     }
 
+    @ApiOperation(value = "Update product", tags = "update", httpMethod = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully update product record"),
+            @ApiResponse(code = 400, message = "Validation failed"),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public UpdateProductResponse update(@PathVariable("id") Long id,
@@ -51,12 +71,24 @@ public class ProductController {
         return productService.searchByName(name);
     }
 
+    @ApiOperation(value = "Get product by id", tags = "getById", httpMethod = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully get product record by id"),
+            @ApiResponse(code = 404, message = "Product not found error"),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('USER')")
     public Product getById(@PathVariable("id") Long id){
         return productService.getById(id);
     }
 
+    @ApiOperation(value = "Delete product by id", tags = "delete", httpMethod = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Successfully delete product by id"),
+            @ApiResponse(code = 404, message = "Product not found error"),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
